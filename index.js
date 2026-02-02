40bf41bb914af3b8f1a450a0a2b334e5f3b23494
AloyeBolu <aloyebolu5@gmail.com>
2026-02-02 10:06:51 +0100

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/docs', docsRouter);
+app.use('/bridgegap/docs', docsRouter);
 
 app.get('/health', (req, res) => {
   res.send('OK');
